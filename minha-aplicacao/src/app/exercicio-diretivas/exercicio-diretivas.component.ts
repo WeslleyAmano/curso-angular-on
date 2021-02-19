55f07e342a26f00cbd01cdd962436b4afb279710
Weslley Amano <weslley.amano@hotmail.com>
2021-02-19 18:21:15 -0300

    feat: exercicios diretivas estruturais

--- minha-aplicacao/src/app/exercicio-diretivas/exercicio-diretivas.component.ts
+++ minha-aplicacao/src/app/exercicio-diretivas/exercicio-diretivas.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+import { MEMES_AGRUPADOS_POR_CATEGORIA } from './exercicio-diretivas.constants';
+
 @Component({
   selector: 'app-exercicio-diretivas',
   templateUrl: './exercicio-diretivas.component.html',
   styleUrls: ['./exercicio-diretivas.component.scss']
 })
 export class ExercicioDiretivasComponent {
+
+  MEMES_AGRUPADOS_POR_CATEGORIA = MEMES_AGRUPADOS_POR_CATEGORIA;
+  Referencia = 'https://raw.githubusercontent.com/vitorfgsantos/angular-memes-diretivas/master/images';
+
 deveExibir = false;
 
 listaFrutas = [
@@ -13,7 +19,7 @@ listaFrutas = [
   'laranja',
   'mamão',
   'uva',
-]
+];
 
 trocarCond(){
   this.deveExibir = !this.deveExibir;
